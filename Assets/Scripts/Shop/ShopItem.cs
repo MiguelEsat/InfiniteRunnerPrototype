417f3194aa0be1fb563d8b380b43766a2f51caf5
MiguelEsat <valeroce@esat-alumni.com>
2025-04-14 15:09:27 +0200

    Implemented shop

---
+++ Assets/Scripts/Shop/ShopItem.cs
@@ -0,0 +1,33 @@
+using System;
+using UnityEngine;
+using UnityEngine.U2D;
+
+[Serializable]
+public class ShopItem
+{
+    public string itemName;
+    public string description;
+    public float price;
+    public int level;
+    public float priceMultiplier;
+    public Action onPurchase;
+    public Sprite icon;
+
+    public ShopItem(string name, string desc, float cost, Action effect, Sprite sprite, float multiplier = 1.5f)
+    {
+        itemName = name;
+        description = desc;
+        price = cost;
+        onPurchase = effect;
+        priceMultiplier = multiplier;
+        icon = sprite;
+        level = 0;
+    }
+
+    public void Buy()
+    {
+        level++;
+        price = (float)Math.Round(price * priceMultiplier, 2);
+        onPurchase?.Invoke();
+    }
+}
---
+++ Assets/Scripts/Shop/ShopItemButton.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Unity.VisualScripting;
+
+public class ShopItemButton : MonoBehaviour
+{
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI priceText;
+    public TextMeshProUGUI levelText;
+    public TextMeshProUGUI descriptionText;
+    public Image iconImage;
+    public Button buyButton;
+    public Image backgroundImage;
+
+    private ShopItem currentItem;
+
+
+    public void Setup(ShopItem item)
+    {
+        currentItem = item;
+
+
+        iconImage.sprite = item.icon;
+        priceText.text = "Price: " + item.price.ToString("F1");
+        levelText.text = "Lvl: " + item.level;
+
+        buyButton.onClick.RemoveAllListeners();
+        buyButton.onClick.AddListener(BuyItem);
+    }
+    void Update()
+    {
+        UpdateButtonUI();
+    }
+    void BuyItem()
+    {
+        if (CoinManager.instance.SpendCoins(currentItem.price))
+        {
+            currentItem.Buy();
+            UpdateButtonUI();
+            CoinManager.instance.UpdateTextOnScreen();
+        }
+        else
+        {
+            Debug.Log("No tienes suficientes monedas");
+        }
+    }
+
+    void UpdateButtonUI()
+    {
+
+        priceText.text = "Price: " + currentItem.price.ToString("F1");
+        levelText.text = "Lvl: " + currentItem.level;
+        descriptionText.text = "Cps: " + currentItem.description;
+
+        bool canAfford = CoinManager.instance.score_ >= currentItem.price;
+
+        if (canAfford)
+        {
+            buyButton.image.color = Color.green;
+        }
+        else
+        {
+            buyButton.image.color = Color.red;
+        }
+    }
+
+
+}
